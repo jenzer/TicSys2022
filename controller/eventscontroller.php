3c199e5a78384934886e32935310abeffbe0d428
Marc Jenzer <marc.jenzer@semabit.ch>
2017-11-14 17:54:35 +0100

    use csv

--- controller/eventscontroller.php
+++ controller/eventscontroller.php
@@ -1,14 +1,12 @@
 <?php
-
-$eventlist = array();
-
-include_once 'resources/eventlist.php';
-
-foreach ($eventlist as $anEvent) {
-    echo "<div class=\"event-info\">\n";
-    echo "<h2>{$anEvent['title']}</h2>\n";
-    echo "<h3>{$anEvent['date']}</h3>\n";
-    echo "<p>{$anEvent['description']}</p>\n";
-    echo "</div>\n";
+$handle = fopen("{$_SERVER['DOCUMENT_ROOT']}/resources/eventlist.csv", "r");
+if ($handle !== false) {
+    while(($data = fgetcsv($handle)) !== false) {
+        echo "<div class=\"event-info\">\n";
+        echo "<h2>{$data[0]}</h2>\n";
+        echo "<h3>{$data[1]}</h3>\n";
+        echo "<p>{$data[2]}</p>\n";
+        echo "</div>\n";
+    } 
+    fclose($handle);
 }
-
