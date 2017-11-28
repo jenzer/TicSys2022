cb703e379ba1c4a544ecc39a40e7258ccb4a2049
Marc Jenzer <marc.jenzer@semabit.ch>
2017-11-28 08:28:50 +0100

    add validations

--- controller/contactcontroller.php
+++ controller/contactcontroller.php
@@ -1,21 +1,33 @@
 <?php
 if (preg_match('@/success$@i', $_SERVER['REQUEST_URI'])) {
+    // show confirmation page
     echo "<p>Vielen Dank für Ihre Anfrage, wir setzen uns sobald wie möglich mit Ihnen in Verbindung.</p>";
 } else {
     if ((!empty($_POST['contact'])) && (empty($_POST['name']))) {
-        $message = "Vom Benutzer erfasste Daten:\n\n";
-        foreach ($_POST as $key => $value) {
-            $message .= "$key: $value\n";
+        // Form submitted by human
+        // validate form
+        $valid = true;
+        $valid &=!empty($_POST['subject']);
+        $valid &=!empty($_POST['message']);
+        $valid &=!empty($_POST['email']);
+
+        if ($valid) {
+            $message = "Vom Benutzer erfasste Daten:\n\n";
+            foreach ($_POST as $key => $value) {
+                $message .= "$key: $value\n";
+            }
+            mail("jem@semabit.ch", "TicSys - Kontaktformular", $message);
+            header("HTTP/1.1 303 See Other");
+            header("Location: " . URI_KONTAKT . "/success");
+            exit();
+        } else {
+            echo "<p>Bitte füllen Sie alle markierten Felder aus.</p>";
         }
-        mail("jem@semabit.ch", "TicSys - Kontaktformular", $message);
-        header("HTTP/1.1 303 See Other");
-        header("Location: " . URI_KONTAKT . "/success");
-        exit();
     }
     ?>
     <form id="contactform" action="<?php echo URI_KONTAKT ?>" method="post" name="contactform">
         <label for="contactform-subject">Betreff</label>
-        <input type="text" id="contactform-subject" name="subject" required autocomplete="off" list="subjects">
+        <input type="text" id="contactform-subject" name="subject" required autocomplete="off" list="subjects" <?php validate('subject', true); ?>>
         <datalist id="subjects">
             <option value="Bestellung">
             <option value="Rechnung">
@@ -25,19 +37,23 @@ if (preg_match('@/success$@i', $_SERVER['REQUEST_URI'])) {
         </datalist>
 
         <label for="contactform-message">Mitteilung</label>
-        <textarea id="contactform-message" name="message" rows="8" cols="50" required></textarea>
+        <textarea id="contactform-message" name="message" rows="8" cols="50" required><?php
+    if (!empty($_POST['message'])) {
+        echo $_POST['message'];
+    }
+    ?></textarea>
 
         <label for="contactform-last_name">Name</label>
-        <input type="text" id="contactform-last_name" name="last_name">
+        <input type="text" id="contactform-last_name" name="last_name" <?php validate('last_name'); ?>>
 
         <label for="contactform-first_name">Vorname</label>
-        <input type="text" id="contactform-first_name" name="first_name">
+        <input type="text" id="contactform-first_name" name="first_name" <?php validate('first_name'); ?>>
 
         <label for="contactform-phone">Telefon-Nr.</label>
-        <input type="tel" id="contactform-phone" name="phone">
+        <input type="tel" id="contactform-phone" name="phone" <?php validate('phone'); ?>>
 
         <label for="contactform-email">Email-Adresse</label>
-        <input type="email" id="contactform-email" name="email" required>
+        <input type="email" id="contactform-email" name="email" required <?php validate('email', true); ?>>
 
         <label for="contactform-newsletter">Newsletter abonnieren</label>
         <input type="checkbox" id="contactform-newsletter" name="newsletter" checked>
@@ -48,5 +64,18 @@ if (preg_match('@/success$@i', $_SERVER['REQUEST_URI'])) {
     </form>
 
     <?php
+}
+
+function validate($key, $required = false) {
+    if (!empty($_POST[$key])) {
+        echo "value=\"{$_POST[$key]}\"";
+    }
+    if ($required) {
+        $class = "required";
+        if ((!empty($_POST['contact'])) && (empty($_POST[$key]))) {
+            $class .= " missing";
+        }
+        echo "class=\"$class\"";
+    }
 }
 ?>
